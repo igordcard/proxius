5064e28cb034689f42e186270721f4460380bf8c
Igor DC <igordcard@gmail.com>
2024-04-18 11:08:05 -0700

    Getting upset at this
    kubectl apply -k config/samples/
    Error from server (InternalError): error when creating "config/samples/": Internal error occurred: failed calling webhook "mproxydef.kb.io": failed to call webhook: Post "https://webhook-service.proxius-system.svc:443/mutate-webhooks-igordc-com-v1-proxydef?timeout=10s": service "webhook-service" not found

--- cmd/main.go
+++ cmd/main.go
@@ -101,10 +101,11 @@ func main() {
 			SecureServing: secureMetrics,
 			TLSOpts:       tlsOpts,
 		},
-		WebhookServer:          webhookServer,
-		HealthProbeBindAddress: probeAddr,
-		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "1cb581b0.igordc.com",
+		WebhookServer:                 webhookServer,
+		HealthProbeBindAddress:        probeAddr,
+		LeaderElection:                enableLeaderElection,
+		LeaderElectionID:              "1cb581b0.igordc.com",
+		DisableWebhookConfigInstaller: true,
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
