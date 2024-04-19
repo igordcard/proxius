bdcd8081ef840119da72a88d0cf5185962b7b63c
Igor DC <igordcard@gmail.com>
2024-04-19 13:06:19 -0700

    Webhook call working now!
    Next is to make it actually do something. Also that annotate isn't
    actually working. Next commit should hopefully mean the completion of
    the core of proxius - finally.

--- cmd/main.go
+++ cmd/main.go
@@ -23,18 +23,18 @@ import (
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
-	corev1 "k8s.io/api/core/v1"
+
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 
 	proxyv1alpha1 "github.com/igordcard/proxius/api/v1alpha1"
 	"github.com/igordcard/proxius/internal/controller"
@@ -150,21 +150,23 @@ func main() {
 	}
 
 	// if os.Getenv("ENABLE_WEBHOOKS") != "false" {
-	if err := builder.WebhookManagedBy(mgr).
-		For(&corev1.Pod{}).
-		WithDefaulter(&podAnnotator{}).
-		Complete(); err != nil {
-		setupLog.Error(err, "unable to create webhook", "webhook", "Pod")
-		os.Exit(1)
-	}
+	// 	err = builder.WebhookManagedBy(mgr).
+	// 		For(&corev1.Pod{}).
+	// 		WithDefaulter(&podAnnotator{}).
+	// 		Complete()
+	// 	if err != nil {
+	// 		setupLog.Error(err, "unable to create webhook", "webhook", "Pod")
+	// 		os.Exit(1)
+	// 	}
 	// }
 
-	// mgr.GetWebhookServer().Register("/mutate-v1-pod", &webhook.Admission{
-	// 	Handler: &podAnnotator{
-	// 		Client:  mgr.GetClient(),
-	// 		decoder: admission.NewDecoder(mgr.GetScheme()),
-	// 	},
-	// })
+	mgr.GetWebhookServer().Register("/mutate-v1-pod", &webhook.Admission{
+		Handler: &PodMutator{
+			Client:  mgr.GetClient(),
+			decoder: admission.NewDecoder(mgr.GetScheme()),
+		},
+	})
+
 	setupLog.Info("starting manager")
 	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
 		setupLog.Error(err, "problem running manager")
--- cmd/webhook.go
+++ cmd/webhook.go
@@ -19,18 +19,41 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/http"
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
 //+kubebuilder:webhook:path=/mutate-v1-pod,mutating=true,failurePolicy=fail,groups="",resources=pods,verbs=create;update,versions=v1,name=mpod.kb.io,admissionReviewVersions=v1,sideEffects=NoneOnDryRun
 
-type podAnnotator struct{}
+type PodMutator struct {
+	Client  client.Client
+	decoder *admission.Decoder
+}
 
-func (a *podAnnotator) Default(ctx context.Context, obj runtime.Object) error {
+func (a *PodMutator) Handle(ctx context.Context, req admission.Request) admission.Response {
+	log := logf.FromContext(ctx)
+	pod := &corev1.Pod{}
+	if err := a.decoder.Decode(req, pod); err != nil {
+		log.Info("Failed to decode Pod", "err", err)
+		return admission.Errored(http.StatusBadRequest, err)
+	}
+
+	if pod.Annotations == nil {
+		pod.Annotations = map[string]string{}
+	}
+	pod.Annotations["example-mutating-admission-webhook"] = "foo"
+	log.Info("Annotated Pod")
+
+	return admission.Allowed("")
+}
+
+func (a *PodMutator) Default(ctx context.Context, obj runtime.Object) error {
 	log := logf.FromContext(ctx)
 	pod, ok := obj.(*corev1.Pod)
 	if !ok {
